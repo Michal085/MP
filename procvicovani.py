66f5c8db687b2134bb710072be25e9940b326400
Michal Plačko <90787029+Michal085@users.noreply.github.com>
2022-02-08 15:09:08 +0100

    Update procvicovani.py

--- procvicovani.py
+++ procvicovani.py
@@ -3,25 +3,25 @@
 #plus_sest = [x+6 for x in hodnoty]
 #print(plus_sest)
 
-#2
-#cars_kilos ={"Sedan": 1500, "Kodiaq": 2000, "Pickup": 2500, "Man": 16000, "Van": 2400, "Semi": 13600, "Bicycle": 7, "Motorcycle": 110}
-#cars_names_up_to_2500 = [car for car in cars_kils if cars_kilos[car]<2500]
-#print(cars_names_up_to_2500)
-#3
-#akcie = ["META", "GOOG", "AMZN", "NTFX", "AAPL"]
-#cena = [100, 130, 160, 299, 120]
-#akcie_cena = {stock:price for stock, price in zip(akcie, cena)}
-#print(akcie_cena)
+2
+cars_kilos ={"Sedan": 1500, "Kodiaq": 2000, "Pickup": 2500, "Man": 16000, "Van": 2400, "Semi": 13600, "Bicycle": 7, "Motorcycle": 110}
+cars_names_up_to_2500 = [car for car in cars_kils if cars_kilos[car]<2500]
+print(cars_names_up_to_2500)
+3
+akcie = ["META", "GOOG", "AMZN", "NTFX", "AAPL"]
+cena = [100, 130, 160, 299, 120]
+akcie_cena = {stock:price for stock, price in zip(akcie, cena)}
+print(akcie_cena)
 
 import requests
 import csv
 from bs4 import BeautifulSoup
-#def filmy_list():
-# text = ""
-#r = requests.get("https://www.cas.sk/clanok/2616795/koronavirus-nadalej-drzi-slovensko-v-pazuroch-vysoky-narast-nakazenych-i-obeti/")
-#soup = BeautifulSoup(r.text, "html.parser")
-#text_field = soup.find("div", {"id" : "introtext mt20"})
-#print(text_field)
+def filmy_list():
+ text = ""
+r = requests.get("https://www.cas.sk/clanok/2616795/koronavirus-nadalej-drzi-slovensko-v-pazuroch-vysoky-narast-nakazenych-i-obeti/")
+soup = BeautifulSoup(r.text, "html.parser")
+text_field = soup.find("div", {"id" : "introtext mt20"})
+print(text_field)
 
 strana = float(input('Zadej stranu čtverce v centimetrech: '))
 cislo_je_spravne = strana > 0
