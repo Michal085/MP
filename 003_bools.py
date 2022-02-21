4dc3f39c45f990adbfa9af2667c004bae126737e
Michal Plačko <90787029+Michal085@users.noreply.github.com>
2022-02-21 14:54:22 +0100

    Update 003_bools.py

--- 003_bools.py
+++ 003_bools.py
@@ -1,4 +1,3 @@
-# Video: https://youtu.be/JWLHUK16WTI
 # Datovy typ bool ma len dve hodnoty True a False
 true_value = True
 false_value = False
@@ -29,10 +28,5 @@ not(x)
 3 < 4 or 5 > 6
 not(3 < 4 and 5 > 6)
 
-# Cvicenie na doma skus uhadnut pravdivostnu hodnotu potom to over v pythone
-not(4 <= 16) and 5 > 7 
-17 != 14 or 4 > 6
-9 == 8 and 3 <= 2
-10 >= 13 and 4 != 3 or 12 < 14
-not(3 > 6) or not(4 == 18 or 7 > 8)
+
 
