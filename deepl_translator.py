3329727b6932f85a0314f947a265e6a3c49f3f6f
Michal Plačko <90787029+Michal085@users.noreply.github.com>
2022-02-23 13:52:57 +0100

    Update deepl_translator.py

--- deepl_translator.py
+++ deepl_translator.py
@@ -1,5 +1,5 @@
 import csv
-with open('Slovakia_Covid_AgTests.csv', newline='', encoding='utf-8') as f:
+with open('Slovakia_Covid_AgTests.csv', newline='' , encoding='utf-8') as f:
     tests = csv.reader(f)
     for test in tests:
         print(test)
