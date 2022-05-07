2aed75c2bf86ba35cbdbffa444f1f0b464d21803
Michal Plačko <90787029+michalplacko@users.noreply.github.com>
2022-05-07 19:08:33 +0200

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -3,7 +3,7 @@ sorted_zvirata = sorted(zvirata, key=lambda x: len(x))
 print(sorted_zvirata)
 
 
-mesta_populace =[(19542209, "New York") ,(4887871, "Alabama"), (1420491, "Hawaii"), (626299, "Vermont"), (1805832, "West Virginia"), (39865590, "California")]
+mesta_populace =[(19542209, "New_York") ,(4887871, "Alabama"), (1420491, "Hawai"), (626299, "Vermont"), (1805832, "West_Virginia"), (39865590 , "California")]
 podle_max_populace = sorted(mesta_populace, key= lambda x: x[0])
 mesta_podle_abecedy = sorted(mesta_populace, key= lambda x: x[1])
 print(podle_max_populace)
