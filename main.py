95eb678b03df714dd34709726631e107810e304c
Michal Plačko <90787029+michalplacko@users.noreply.github.com>
2022-05-16 10:53:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import csv
 import typing as t
 from datetime import date, datetime, timedelta
 from pprint import pprint
-f = open('OpenData_Slovakia_Covid_AgTests_District (1).csv' , 'r')
+f = open('OpenData_Slovakia_Covid_AgTests_District (1).csv' , ' r ')
 obsahSouboru = f.read()
 def load_lines_v2(file_name, 'r' )  asfile:
     with open('OpenData_Slovakia_Covid_AgTests_District (1).csv', 'r') as file:
