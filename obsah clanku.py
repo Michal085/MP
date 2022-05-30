6178f5a565bb03a5e160fb7ae64f1862d4822ef5
Michal Plačko <90787029+michalplacko@users.noreply.github.com>
2022-05-30 09:07:47 +0200

    Update obsah clanku.py

--- obsah clanku.py
+++ obsah clanku.py
@@ -3,7 +3,7 @@ import csv
 from bs4 import BeautifulSoup
 def filmy_list():
 
- obsah_clanku = []
+obsah_clanku = []
 r = requests.get("https://zdopravy.cz/airbus-ma-dalsi-velkou-zakazku-prvni-zakaznik-si-objednal-a350-v-nakladni-verzi-96785/")
 soup = BeautifulSoup(r.text, "html.parser")
 obsah_clanku = soup.find_all("div", id="titleColumn")
