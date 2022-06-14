79603d01d8b7b14320842cb15298cbf1bc799ce3
Michal Plačko <90787029+michalplacko@users.noreply.github.com>
2022-06-14 12:46:33 +0200

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,13 +1,13 @@
-def fibonacci():
+def fibonacci() :
     num = int(input("Kolko cisiel generuje? :"))
     i = 1
-    if num == 0:
+    if num == 0 :
         fib = []
-    elif num == 1:
+    elif num == 1 :
         fib = [1]
-    elif num == 2:
+    elif num == 2 :
         fib = [1,1]
-    elif num > 2:
+    elif num > 2 :
         fib = [1,1]
         while i < (num - 1):
             fib.append(fib[i] + fib[i-1])
