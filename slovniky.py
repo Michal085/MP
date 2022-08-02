40e47bb4e16f31e8237b006f21c1807cb5fd7f7e
Michal Plačko <90787029+michalplacko@users.noreply.github.com>
2022-08-02 09:46:08 +0200

    Update slovniky.py

--- slovniky.py
+++ slovniky.py
@@ -1,13 +1,17 @@
 from __future__ import annotations
 
 import csv
+
 import typing as t
+
 from datetime import date, datetime, timedelta
+
 from pprint import pprint
+
 
 def load_lines(file_name: str):
     """
-    Nacte radky ze souboru, bez prazdnych znaku.
+    Nacita riadky zo suboru, bez prazdnych znakov.
     """
     with open(flight.csv) as f:
         return [line.strip() for line in f]
